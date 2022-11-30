c9b98d6b7534879eb482954e83c5c48d19b94618
Maxim Slobozhanin <slob.max@gmail.com>
2022-12-01 01:52:38 +0400

    separation revision

--- src/__test__/app.test.js
+++ src/__test__/app.test.js
@@ -15,3 +15,37 @@ test('Класс Character, проверка type', () => {
   }
   expect(genErr).toThrow();
 });
+
+test('Метод Character.levelUp', () => {
+  const received = new Bowman('Bowman', 'Bowman');
+  const expected = {
+    name: 'Bowman',
+    type: 'Bowman',
+    health: 100,
+    level: 1,
+    attack: 30,
+    defence: 30,
+  };
+  received.levelUp();
+  expect(received).toEqual(expected);
+});
+
+test('Метод Character.levelUp, выброс ошибки', () => {
+  function genErr() {
+    const received = new Bowman('Bowman', 'Bowman');
+    received.damage(150);
+    received.levelUp();
+    return received;
+  }
+  expect(genErr).toThrow();
+});
+
+test('Метод Character.damage', () => {
+  function calcHealth(points) {
+    const received = new Bowman('Bowman', 'Bowman');
+    received.damage(points);
+    return received.health;
+  }
+  expect(calcHealth(40)).toBe(70);
+  expect(calcHealth(200)).toBe(0);
+});
